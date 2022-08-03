154bf95da9d6c0986db95d2c9bde069b813e0a52
DharsanFrank <dharsanraj7@gmail.com>
2022-08-03 13:04:46 +0530

    03.08.2022 - Java Data Type Assignment Completed.

---
+++ src/MainClass.java
@@ -0,0 +1,70 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Hi I'm Dharsan");
+		
+		
+		System.out.print("Min value of Byte");
+		System.out.println(Byte.MIN_VALUE);
+		System.out.print("Max value of Byte");
+		System.out.println(Byte.MAX_VALUE);
+		System.out.print("Min value of Short");
+		System.out.println(Short.MIN_VALUE);
+		System.out.print("Max value of Short");
+		System.out.println(Short.MAX_VALUE);
+		System.out.print("Min value of Integer");
+		System.out.println(Integer.MIN_VALUE);
+		System.out.print("Max value of Integer");
+		System.out.println(Integer.MAX_VALUE);
+		System.out.print("Min value of Long");
+		System.out.println(Long.MIN_VALUE);
+		System.out.print("Max value of Long");
+		System.out.print(Long.MAX_VALUE);
+		System.out.print("Min value of Float");
+		System.out.println(Float.MIN_VALUE);
+		System.out.print("Max value of Float");
+		System.out.println(Float.MAX_VALUE);
+		System.out.print("Min value of Double");
+		System.out.println(Double.MIN_VALUE);
+		System.out.print("Max value of Double");
+		System.out.println(Double.MAX_VALUE);
+		
+		
+				
+				
+		String companyName = "Capgemini Technology Services India Limited";
+		int emp_id = 46178580;
+		String emp_name = "Dharsan Raj P";
+		String emp_emailId = "dharsan.raj@capgemini.com";
+		String designation = "Associate Consultant";
+		String emp_address = "4/15,11th Cross Shanmuga Nagar,U.k.T - Malai,Trichy - 620102.";
+		String emp_department = "Java Cloud FS";
+		String emp_gender = "Male";
+		byte age = 24;
+		float yearOfExperience = 3.3f;
+		long phoneNumber = 9894403822L;
+		String baseLocation = "Bangalore";
+		
+		System.out.println(companyName);
+		System.out.println(emp_id);
+		System.out.println(emp_name);
+		System.out.println(emp_emailId);
+		System.out.println(designation);
+		System.out.println(emp_address);
+		System.out.println(emp_department);
+		System.out.println(emp_gender);
+		System.out.println(age);
+		System.out.println(yearOfExperience);
+		System.out.println(phoneNumber);
+		System.out.println(baseLocation);
+		
+	
+		
+		
+		
+	}
+
+}
